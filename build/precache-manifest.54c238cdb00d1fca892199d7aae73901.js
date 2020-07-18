5f811cdfba93af07ea9942331ac43fabd29e8967
malikmusa <54061926+malikmusa@users.noreply.github.com>
2020-07-18 15:36:06 +0500

    add build

---
+++ build/precache-manifest.54c238cdb00d1fca892199d7aae73901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e14b78165ddec5a140bb4685ffea9b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb352db0ac394384f5b",
+    "url": "/static/css/main.f62d0057.chunk.css"
+  },
+  {
+    "revision": "5f29d0e61ac57911ca0e",
+    "url": "/static/js/2.44051f9b.chunk.js"
+  },
+  {
+    "revision": "7500210a7a039b154f39c22ba39584ea",
+    "url": "/static/js/2.44051f9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bb352db0ac394384f5b",
+    "url": "/static/js/main.5b7fd98b.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "21e67daa832684781c96c95c933b6f2b",
+    "url": "/static/media/bgImg.21e67daa.jpg"
+  }
+]);
